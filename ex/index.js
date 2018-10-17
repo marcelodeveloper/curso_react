2496b5f5966fa66fd67bf8731a1890f829641084
Marcelo Martins <consulte.desenvolvimento@outlook.com>
2018-10-17 08:38:25 -0300

    ex.4. Inclui operador spread para criação de novo objeto

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,16 @@
-import Pessoa from './pessoa'
+const produto = {
+    nome: 'Caneta Bic Preta',
+    preco: 1.90,
+    desconto: 0.05
+}
 
-const pessoa = new Pessoa('Marcelo')
-console.log(pessoa.toString())
+function clone(objeto){
+    //operador spread
+    //{ } - cria um novo objeto
+    return { ...objeto }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Caneta Bic Azul'
+
+console.log(produto,novoProduto)
